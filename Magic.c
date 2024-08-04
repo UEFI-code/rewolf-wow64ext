134a0a4f7b083461ebd3edaa1dcb514b7801e817
SuperHacker UEFI <zhuoshizhang@hotmail.com>
2024-08-04 09:19:08 +0800

    Update Magic.c

--- Magic.c
+++ Magic.c
@@ -3,15 +3,17 @@
 #define X64_Start_with_CS(_cs) \
     { \
     EMIT(0x6A) EMIT(_cs)                         /*  push   _cs             */ \
-    EMIT(0xE8) EMIT(0) EMIT(0) EMIT(0) EMIT(0)   /*  call   $+5             */ \
+    EMIT(0xE8) EMIT(0) EMIT(0) EMIT(0) EMIT(0)   /*  call   $+5 (Bias is 0 actually, just For Save EIP) */ \
     EMIT(0x83) EMIT(4) EMIT(0x24) EMIT(5)        /*  add    dword [esp], 5  */ \
     EMIT(0xCB)                                   /*  retf                   */ \
+    // x64 code ...
     }
 
 #define X64_End_with_CS(_cs) \
     { \
-    EMIT(0xE8) EMIT(0) EMIT(0) EMIT(0) EMIT(0)                                 /*  call   $+5                   */ \
+    EMIT(0xE8) EMIT(0) EMIT(0) EMIT(0) EMIT(0)                                 /*  call   $+5 (Our Code is Still 32bit Addr) even in x64 Mode  */ \
     EMIT(0xC7) EMIT(0x44) EMIT(0x24) EMIT(4) EMIT(_cs) EMIT(0) EMIT(0) EMIT(0) /*  mov    dword [rsp + 4], _cs  */ \
     EMIT(0x83) EMIT(4) EMIT(0x24) EMIT(0xD)                                    /*  add    dword [rsp], 0xD      */ \
     EMIT(0xCB)                                                                 /*  retf                         */ \
+    // x86 code ...
     }
